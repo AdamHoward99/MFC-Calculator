390b9f1c82b36801ca41d0f243ecc1c55ccf311f
AdamHoward99 <74617187+AdamHoward99@users.noreply.github.com>
2021-09-24 15:14:35 +0100

    Added initial project settings and basic UI

---
+++ MFCCalculator/MFCCalculatorDoc.cpp
@@ -0,0 +1,138 @@
+
+// MFCCalculatorDoc.cpp : implementation of the CMFCCalculatorDoc class
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS can be defined in an ATL project implementing preview, thumbnail
+// and search filter handlers and allows sharing of document code with that project.
+#ifndef SHARED_HANDLERS
+#include "MFCCalculator.h"
+#endif
+
+#include "MFCCalculatorDoc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CMFCCalculatorDoc
+
+IMPLEMENT_DYNCREATE(CMFCCalculatorDoc, CDocument)
+
+BEGIN_MESSAGE_MAP(CMFCCalculatorDoc, CDocument)
+END_MESSAGE_MAP()
+
+
+// CMFCCalculatorDoc construction/destruction
+
+CMFCCalculatorDoc::CMFCCalculatorDoc() noexcept
+{
+	// TODO: add one-time construction code here
+
+}
+
+CMFCCalculatorDoc::~CMFCCalculatorDoc()
+{
+}
+
+BOOL CMFCCalculatorDoc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: add reinitialization code here
+	// (SDI documents will reuse this document)
+
+	return TRUE;
+}
+
+
+
+
+// CMFCCalculatorDoc serialization
+
+void CMFCCalculatorDoc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: add storing code here
+	}
+	else
+	{
+		// TODO: add loading code here
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// Support for thumbnails
+void CMFCCalculatorDoc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// Modify this code to draw the document's data
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// Support for Search Handlers
+void CMFCCalculatorDoc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// Set search contents from document's data.
+	// The content parts should be separated by ";"
+
+	// For example:  strSearchContent = _T("point;rectangle;circle;ole object;");
+	SetSearchContent(strSearchContent);
+}
+
+void CMFCCalculatorDoc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = nullptr;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != nullptr)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// CMFCCalculatorDoc diagnostics
+
+#ifdef _DEBUG
+void CMFCCalculatorDoc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CMFCCalculatorDoc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CMFCCalculatorDoc commands
